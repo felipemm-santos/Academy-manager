631fb3c5763dc4bdbfc31b7ce60a44d2aa01c368
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-07-19 15:04:53 -0300

    Add script to ask confimation when deleting instructors or members

---
+++ public/scripts/delete-form.js
@@ -0,0 +1,8 @@
+const formDelete = document.querySelector('#form-delete');
+
+formDelete.addEventListener('submit', (event) => {
+  const confirmation = confirm('Deseja deletar?');
+  if (!confirmation) {
+    event.preventDefault();
+  }
+});
