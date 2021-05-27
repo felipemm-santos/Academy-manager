257f4c8c41204945e2586b4abd66ebb21bb81cfd
Felipe Morais Mariano dos Santos <62776052+felipemm-santos@users.noreply.github.com>
2021-05-27 19:03:54 -0300

    Add filter per name or services

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -4,9 +4,17 @@ const { getAge, date } = require("../../lib/utils");
 
 module.exports = {
   index(req, res) {
-    Instructor.all(function (instructors) {
-      return res.render("instructors/index", { instructors });
-    });
+    const { filter } = req.query;
+
+    if (filter) {
+      Instructor.findBy(filter, function (instructors) {
+        return res.render("instructors/index", { instructors, filter });
+      });
+    } else {
+      Instructor.all(function (instructors) {
+        return res.render("instructors/index", { instructors });
+      });
+    }
   },
 
   create(req, res) {
